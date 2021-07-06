c85f954c95084f2febca876b649ccf7bf04ab487
elves brito <elvesbd@yahoo.com>
2021-07-05 21:09:41 -0300

    configuration global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/global';
+
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <div className="App" />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>My contacts</h1>
+    </ThemeProvider>
   );
 }
 
