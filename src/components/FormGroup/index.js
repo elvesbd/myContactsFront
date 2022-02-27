785cec94fa32d566d6b09f2513cb658ba79a9162
elvesbd <elvesbd@yahoo.com>
2022-02-27 19:22:07 -0300

    create component spinner

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import Spinner from '../Spinner';
 
 import { Container } from './styles';
 
@@ -9,7 +10,11 @@ export default function FormGroup({ children, error, isLoading }) {
       <div className="form-item">
         {children}
 
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
