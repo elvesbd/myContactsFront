fef7fb215fe4eb6e3142e7bcef4b10c94908950e
elvesbd <elvesbd@yahoo.com>
2022-02-27 16:19:20 -0300

    create categories service

---
+++ src/services/CategoriesService.js
@@ -0,0 +1,17 @@
+import HttpClient from './utils/HttpClient';
+
+class CategoriesService {
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
+
+  async listCategories() {
+    return this.httpClient.get('/categories');
+  }
+  // falta implementar
+  /* async createCategories(contact) {
+    return this.httpClient.get('/contacts', contact);
+  } */
+}
+
+export default new CategoriesService();
