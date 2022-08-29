c8e24e26f6252b23aaf380ed847dce4dfc70cb51
elvesbd <elvesbd@yahoo.com>
2022-08-29 14:23:44 -0300

    feat: implementing event listeners removal method

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -20,14 +20,38 @@ export default class EventManager {
       listener(payload);
     });
   }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+
+    this.listeners[event] = filteredListeners;
+  }
 }
 
 const toastEventManager = new EventManager();
 
-toastEventManager.on('addtoast', (payload) => {
-  console.log('addtoast', payload);
-});
+function addToast1(payload) {
+  console.log('addtoast listener 1', payload);
+}
+function addToast2(payload) {
+  console.log('addtoast listener 2', payload);
+}
+
+toastEventManager.on('addtoast', addToast1);
+toastEventManager.on('addtoast', addToast2);
 
 toastEventManager.emit('addtoast', { type: 'danger', text: 'Texto' });
+
+toastEventManager.removeListener('addtoast', addToast1);
+
+toastEventManager.emit('addtoast', 'Depois de remover...');
 
 console.log(toastEventManager);
