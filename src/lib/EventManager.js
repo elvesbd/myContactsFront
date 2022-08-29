d4129a2f14132493695da3518453d58da3c96b21
elvesbd <elvesbd@yahoo.com>
2022-08-29 14:05:24 -0300

    feat: creating methods to add listeners and emit events

---
+++ src/lib/EventManager.js
@@ -0,0 +1,33 @@
+export default class EventManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(listener);
+  }
+
+  emit(event, payload) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(payload);
+    });
+  }
+}
+
+const toastEventManager = new EventManager();
+
+toastEventManager.on('addtoast', (payload) => {
+  console.log('addtoast', payload);
+});
+
+toastEventManager.emit('addtoast', { type: 'danger', text: 'Texto' });
+
+console.log(toastEventManager);
