76438db30bc688165f85ce2ba1e5c58902fe86fd
elvesbd <elvesbd@yahoo.com>
2022-08-29 18:54:16 -0300

    feat: implementing the functionality to remove Toasts

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -18,10 +18,18 @@ export default function ToastContainer() {
     };
   }, []);
 
+  function handleRemoveMessage(id) {
+    setMessages((prevState) => prevState.filter((message) => message.id !== id));
+  }
+
   return (
     <Container>
       {messages.map((message) => (
-        <ToastMessage key={message.id} text={message.text} type={message.type} />
+        <ToastMessage
+          key={message.id}
+          message={message}
+          onRemoveMessage={handleRemoveMessage}
+        />
       ))}
     </Container>
   );
