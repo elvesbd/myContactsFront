c58ac9e76a646e3b4ef4e6cc9cbd48f8b8e339f3
elvesbd <elvesbd@yahoo.com>
2022-08-30 19:15:07 -0300

    feat: clearing form data after submission

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -17,7 +17,6 @@ export default function NewContact() {
       toast({
         type: 'success',
         text: 'Contato cadastrado com sucesso!',
-        duration: 3000,
       });
     } catch (error) {
       toast({
