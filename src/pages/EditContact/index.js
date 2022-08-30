771934699cc4a03d200c1314dfb0ea1364751f18
elvesbd <elvesbd@yahoo.com>
2022-08-30 19:43:32 -0300

    feat: loading contact data on edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,14 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContacts() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+        setIsLoading(false);
+      } catch (error) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContacts();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
         title="Editar Elves Brito"
       />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
