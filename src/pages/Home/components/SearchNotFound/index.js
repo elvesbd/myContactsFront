9610f95cfc507da5a8263d376b8b8099cdfc1b4f
Elves Brito <elvesbd@yahoo.com>
2022-12-02 20:25:25 -0300

    feat: create SearchNotFound component

---
+++ src/pages/Home/components/SearchNotFound/index.js
@@ -0,0 +1,20 @@
+import Proptypes from 'prop-types';
+import { Container } from './styles';
+import magnifierQuestion from '../../../../assets/images/magnifier-question.svg';
+
+export default function SearchNotFound({ searchTerm }) {
+  return (
+    <Container>
+      <img src={magnifierQuestion} alt="Magnifier question" />
+      <span>
+        Nenhum resultado foi encontrado para
+        {' '}
+        <strong>{searchTerm}</strong>
+      </span>
+    </Container>
+  );
+}
+
+SearchNotFound.propTypes = {
+  searchTerm: Proptypes.string.isRequired,
+};
