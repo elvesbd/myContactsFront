dc4fafe7f748127fa953c0efea2588a6c52d7886
elvesbd <elvesbd@yahoo.com>
2023-11-15 17:08:51 -0300

    chore: remove imports does not used

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import { Routes, Route, useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NewContact from './pages/NewContact';
